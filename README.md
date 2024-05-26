3709f5e40d55d311467b0ebab1ace1cfbbe23173
bogdan <b.colta@student.utwente.nl>
2024-05-26 05:43:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FT-MOEA-CM
+
+The approach presented in this algorithm aims to infer fault tree models from given failure data sets. Based on the implementation of FT-MOEA, FT-MOEA-CM introduces the use of confusion matrix metrics instead of metrics based on Minimal Cut Sets.
---
+++ code/ft_learn/moea/time_tracker.py
@@ -0,0 +1,28 @@
+import time
+import dill
+import pandas as pd
+
+class TimeTracker:
+    def __init__(self,metric_config='11100000000000000000000', dataset="dataset", log_file="timestamps.dill"):
+        self.log_file = log_file
+        self.last_timestamp_start = {}
+        self.timestamps = {}
+        metric_config = [-int(x) for x in metric_config]
+        processed_metric_config = "".join(map(str, metric_config))
+        self.timestamps["metric_config"] = processed_metric_config
+        self.timestamps["dataset"] = dataset
+    
+    def start_timer(self, segment_name):
+        self.last_timestamp_start[segment_name] = time.time()
+    
+    def end_timer(self, segment_name):
+        if (not segment_name in self.timestamps):
+            self.timestamps[segment_name] = []
+        start_time = self.last_timestamp_start[segment_name]
+        self.timestamps[segment_name].append(time.time() - start_time)
+        
+        if segment_name == "pareto_sorting":
+            with open("./timestamps/" + self.log_file + ".dill", "wb") as file:
+                print(self.timestamps)
+                dill.dump(pd.DataFrame(self.timestamps), file)
+                # dill.dump(self.timestamps, file)
